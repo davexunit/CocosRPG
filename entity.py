5d09b05b69e36aeca083ae4d48321658a84cc634
David Thompson <davexunit@gmail.com>
2011-09-18 13:03:00 -0400

    Committing before branch to try to fix this mess.

--- entity.py
+++ entity.py
@@ -23,14 +23,6 @@ class MapActor(cocos.cocosnode.CocosNode):
     def get_rect(self):
         return cocos.rect.Rect(self.x, self.y, self.width, self.height)
 
-class Component(pyglet.event.EventDispatcher):
-    def __init__(self):
-        super(Component, self).__init__()
-        self.parent = None
-
-    def attach(self, parent):
-        self.parent = parent
-
 class DialogComponent(object):
     def __init__(self, text):
         self.text = text
@@ -48,11 +40,13 @@ def load_sign(map, name, x, y, width, height, properties):
     return sign
 
 class TriggerComponent(EventDispatcher):
-    def __init__(self):
-        self.trigger_list = None
+    def __init__(self, parent, trigger_list):
+        self.parent = parent
+        self.trigger_list = trigger_list
 
     def check_triggers(self):
         pass
+
 TriggerComponent.register_event_type('on_actor_enter')
 TriggerComponent.register_event_type('on_actor_exit')
 
@@ -74,14 +68,14 @@ class Portal(MapActor):
         super(Portal, self).__init__(id)
         self.map_file = map_file
         self.exit_id = exit_id
-        self.trigger = TriggerComponent()
+        self.trigger = TriggerComponent(self, None)
         self.trigger.push_handlers(self.on_actor_enter)
     
     def on_actor_enter(self, actor):
         # Only the player can use portals
         if actor == self.parent.player:
             map = loadmap.load(self.map_file, None)
-            map['objects'].add_object(actor)
+            map['actors'].add_object(actor)
             map.player = actor
             actor.position = map.find_actor(self.exit_id).position
             director.replace(map)
@@ -98,24 +92,45 @@ class CollisionComponent(EventDispatcher):
     def __init__(self, parent, collidable=True):
         self.parent = parent
         self.collidable = collidable
+
+    def check_collision(self, x, y):
+        pass
+CollisionComponent.register_event_type('on_collision')
+
+class ActorCollisionComponent(CollisionComponent):
+    def __init__(self, parent, collidable=True):
+        super(ActorCollisionComponent, self).__init__(parent, collidable)
         self.collision_list = None
 
     def check_collision(self, x, y):
         # Ignore collision test if collidable flag is not set
         if not self.collidable:
             return False
-        # Collision test
+        # Check for collision with other collision components
         rect = cocos.rect.Rect(x, y, self.parent.width, self.parent.height)
-        if self.collision_list == None:
-            return False
         for c in self.collision_list:
-            if self != c and self.parent.get_rect().intersects(c.parent.get_rect()):
+            if self != c and c.collidable and rect.intersects(c.parent.get_rect()):
                 # Dispatch collision events
                 self.dispatch_event('on_collision', c.parent)
                 c.dispatch_event('on_collision', self.parent)
                 return True
         return False
-CollisionComponent.register_event_type('on_collision')
+
+class MapCollisionComponent(CollisionComponent):
+    def __init__(self, parent, collidable=True):
+        super(MapCollisionComponent, self).__init__(parent, collidable)
+        self.collision_map = None
+
+    def check_collision(self, x, y):
+        # Ignore collision test if collidable flag is not set
+        if not self.collidable:
+            return False
+        rect = cocos.rect.Rect(x, y, self.parent.width, self.parent.height)
+        for cell in self.collision_map.get_in_region(*(rect.bottomleft + rect.topright)):
+            if cell.tile != None:
+                self.dispatch_event('on_collision', cell)
+                return True
+        return False
 
 class MoveComponent(EventDispatcher, cocos.actions.Move):
     def init(self, speed, direction='south'):
@@ -153,7 +168,7 @@ class MoveComponent(EventDispatcher, cocos.actions.Move):
     def update_velocity(self):
         if self.walking:
             vx, vy = self.get_direction_vector()
-            self.target.velocity = (vx * self.speed, vy * self.speed)
+            self.target.velocity = (vx * self.speed * 1.0, vy * self.speed * 1.0)
         else:
             self.cancel_movement()
 
@@ -175,9 +190,17 @@ class MoveComponent(EventDispatcher, cocos.actions.Move):
         self.cancel_movement()
 
     def step(self, dt):
+        # Request movement (movement can be stopped by event handler)
         vx, vy = self.target.velocity
-        self.dispatch_event('on_request_move', vx * dt, vy * dt)
+        if vx != 0 or vy != 0:
+            self.dispatch_event('on_request_move', vx * dt, vy * dt)
         super(MoveComponent, self).step(dt)
+        self.target.x = int(self.target.x)
+        self.target.y = int(self.target.y)
+        # If movement hasn't been stopped then dispatch on_move event
+        vx, vy = self.target.velocity
+        if vx != 0 or vy != 0:
+            self.dispatch_event('on_move', self.target.x, self.target.y)
 MoveComponent.register_event_type('on_walk')
 MoveComponent.register_event_type('on_stop')
 MoveComponent.register_event_type('on_change_direction')
@@ -189,67 +212,76 @@ class InteractComponent(EventDispatcher):
         self.parent = parent
         self.span = span
         self.reach = reach
+        self.interact_list = None
 
     def interact(self, direction):
-        pass
+        # Create bounding box based on direction that the actor is facing
+        rect = self.parent.get_rect()
+        if direction == 'north':
+            rect.y += rect.height
+            rect.height = self.reach
+        elif direction == 'south':
+            rect.y -= rect.height + self.reach
+            rect.height = self.reach
+        elif direction == 'east':
+            rect.x += rect.width
+            rect.width = self.reach
+        elif direction == 'west':
+            rect.x -= rect.width + self.reach
+            rect.width = self.reach
+        # Check for rect collisions with other registered interaction components
+        for i in self.interact_list:
+            pass
 InteractComponent.register_event_type('on_interact')
 
 class Character(MapActor):
-    def __init__(self, id, anims, sprite_offset, move):
+    def __init__(self, id, anims, sprite_offset, move, collision_map, collision_list):
         super(Character, self).__init__(id)
         self.anims = anims
+        # Sprite component
         self.sprite = cocos.sprite.Sprite(anims['stand_south'])
         self.sprite.image_anchor = (0, 0)
         self.sprite.position = sprite_offset
         self.add(self.sprite)
+        # Movement component
         self.action = self.do(move)
         self.move = self.actions[self.actions.index(self.action)]
         self.move.push_handlers(self)
-        self.collision = CollisionComponent(self)
+        # Collide with actors and the collision map
+        self.actor_collision = ActorCollisionComponent(self)
+        self.map_collision = MapCollisionComponent(self)
+        self.interact = InteractComponent(self, 10, 32)
+
+    def on_enter(self):
+        super(Character, self).on_enter()
+        #self.parent.add_actor_collision(self.actor_collision)
+        #self.parent.add_map_collision(self.map_collision)
+        #self.parent.add_interact(self.interact)
+
+    def on_exit(self):
+        super(Character, self).on_exit()
+        #self.parent.remove_actor_collision(self.actor_collision)
+        #self.parent.remove_map_collision(self.map_collision)
+        #self.parent.remove_interact(self.interact)
 
     def on_walk(self):
-        self.sprite.image = self.anims['walk_' + self.move.direction]
+        self.update_animation()
 
     def on_stop(self):
-        self.sprite.image = self.anims['stand_' + self.move.direction]
+        self.update_animation()
 
     def on_change_direction(self, direction):
+        self.update_animation()
+
+    def update_animation(self):
         prefix = 'walk_' if self.move.walking else 'stand_'
         self.sprite.image = self.anims[prefix + self.move.direction]
 
     def on_request_move(self, dx, dy):
-        if self.collision.check_collision(self.x + dx, self.y + dy):
-            self.move.cancel_movement()
-
-    def on_move(self, x, y):
         pass
+        #x, y = self.x + dx, self.y + dy
+        #if self.map_collision.check_collision(x, y) or self.actor_collision.check_collision(x, y):
+        #    self.move.cancel_movement()
 
-class ObjectLayer(cocos.layer.ScrollableLayer):
-    def __init__(self, id=''):
-        super(ObjectLayer, self).__init__()
-        self.id = id
-        self.objects = []
 
-    def add_object(self, obj):
-        self.objects.append(obj)
-        self.add(obj)
-        # If the object is a sprite then add it to the batch for drawing
-        #if isinstance(obj, cocos.sprite.Sprite):
-        #    self.add(obj, z=-obj.y)
-    
-    def remove_object(self, obj):
-        self.objects.remove(obj)
-        # If the object is a sprite then remove it from the batch
-        if isinstance(obj, cocos.sprite.Sprite):
-            self.remove(obj)
-
-    def get_objects(self):
-        return self.objects
-
-    def get_in_region(self, rect):
-        objs = []
-        for o in self.objects:
-            if o.get_hitbox().intersects(rect):
-                objs.append(o)
-        return objs
 
